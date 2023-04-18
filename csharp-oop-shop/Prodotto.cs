d18b09cc26532c69227cd32a8a1d6621edf6ddb5
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-18 17:31:24 +0200

    Bonus

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -18,27 +18,148 @@ namespace csharp_oop_shop
 			}
 		}
 
-
-		private string name;
-		public int Name
+		private string namePlusCode;
+		public string NamePlusCode
 		{
 			get
 			{
-				return name;
+				return namePlusCode;
 			}
 			set
 			{
+				namePlusCode = value;
+			}
+		}
+
+		private string formattedCode;
+		public string FormattedCode
+		{
+			get
+			{
+				return formattedCode;
+			}
+		}
+
+
+		private string name;
+		public string Name
+		{
+			get{
+				return name;
+			}
+			set{
 				name = value;
 			}
 		}	
 
 
 		private string description;
+		public string Description
+		{
+			get{
+				return description;
+			}
+			set{
+				description = value; 
+			}
+		}
 
 
 		private float price;
+		public float Price
+		{
+			get
+			{
+				return price;
+			}
+			set
+			{
+				price = value;
+			}
+		}
 
 
-		private int tax;
+		private float tax;
+		public float Tax
+		{
+			get
+			{
+				return tax;
+			}
+			set
+			{
+				tax = value;
+			}
+		}
+
+		private float priceWithTax;
+		public float PriceWithTax
+		{
+			get
+			{
+				return priceWithTax;
+			}
+			set
+			{
+				priceWithTax = value;
+			}
+		}
+
+		//CONSTRUCTOR
+		public Prodotto(string name, string description, float price)
+		{
+			this.Name = name;
+			this.Description = description;
+			this.Price = price;
+			this.Tax = 1.22f;
+			this.code = RandomNumber();
+			this.formattedCode = RandomFormattedNumber();
+			this.NamePlusCode = nameCode();
+			this.PriceWithTax = taxedPrice();
+		}
+
+
+		//METHODS
+		private int RandomNumber()
+		{
+			Random randomGenerator = new Random();
+			int randomNumber = randomGenerator.Next(10000000);
+			
+
+			return randomNumber;
+		}
+
+		private string RandomFormattedNumber()
+		{
+			Random randomGenerator = new Random();
+			int randomNumber = randomGenerator.Next(10000000);
+
+			string zero = "";
+			for(int i = 0; i < randomNumber.ToString().Length; i++)
+			{
+				zero += "0";
+			}
+
+			zero = zero + Code;
+			return zero;
+		}
+
+		public float basePrice()
+		{
+			return price;
+		}
+
+
+		public float taxedPrice()
+		{
+			float priceWithTax = price * tax;
+			return priceWithTax;
+		}
+
+		public string nameCode()
+		{
+			namePlusCode = name + code;
+			return namePlusCode;
+		}
 	}
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using csharp_oop_shop;
+
+Prodotto prodottoUno = new Prodotto("Prodottolino", "Primo prodotto", 500f);
+
+Console.WriteLine(prodottoUno.PriceWithTax);
+Console.WriteLine(prodottoUno.NamePlusCode);
+Console.WriteLine(prodottoUno.FormattedCode);
+Console.WriteLine(prodottoUno.Price);
