ab8be98320cd66711fa209e6b2d1f27ca9443900
JordanSgrigna <kaliwyrdiscord@gmail.com>
2023-04-18 15:42:59 +0200

    Product class + random number method

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+	public class Prodotto
+	{
+		//ATTRIBUTES + PROPERTIES
+		private int code;
+		public int Code
+		{
+			get
+			{
+				return code;
+			}
+		}
+
+
+		private string name;
+		public int Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				name = value;
+			}
+		}	
+
+
+		private string description;
+
+
+		private float price;
+
+
+		private int tax;
+	}
+}
